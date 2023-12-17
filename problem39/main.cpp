91d5dcf0b0f946a5e454d4b06237d3b1e088e930
Kevin Luke <kevzawinning@gmail.com>
2023-12-17 21:36:07 +1300

    Problem39. Much improved run time

--- problem39/main.cpp
+++ problem39/main.cpp
@@ -1,12 +1,13 @@
 #include <gtest/gtest.h>
 #include <vector>
+
 using namespace std;
 
 namespace {
   class Solution {
     public:
 
-      void combinationSumRec(vector<int> nums, int target, int i, vector<int> solution, int sum,  vector<vector<int>>& solutions)
+      void combinationSumRec(vector<int>& nums, int target, int i, vector<int> &solution, int sum,  vector<vector<int>>& solutions)
       {
         // Out of numbers
         if (i >= nums.size()) {
@@ -24,15 +25,18 @@ namespace {
           return;
         }
 
-        vector<int> newSolution(solution.begin(), solution.end());
-        newSolution.push_back(nums[i]);
-        combinationSumRec(nums, target, i, newSolution, sum + nums[i], solutions);
+        solution.push_back(nums[i]);
+        combinationSumRec(nums, target, i, solution, sum + nums[i], solutions);
+        solution.pop_back();
+
         combinationSumRec(nums, target, i + 1, solution, sum, solutions);
       }
+
       vector<vector<int> > combinationSum(vector<int> nums, int target)
       {
         vector<vector<int> > solution;
-        combinationSumRec(nums, target, 0, vector<int>(), 0, solution);
+        std:vector<int> s;
+        combinationSumRec(nums, target, 0, s, 0, solution);
         return solution;
       }
   };
